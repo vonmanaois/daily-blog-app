c697eb3d92fdf47f0685ed2c437a3e8900d95cd2
Von <von.manaois@gmail.com>
2026-01-29 10:03:25 -0500

    Initial Project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <main>
+      <Post author="John Doe" text="I will be a developer" />
+    </main>
+  );
+}
+export default App;
---
+++ src/components/Post.jsx
@@ -0,0 +1,10 @@
+import classes from "./Post.module.css";
+function Post(props) {
+  return (
+    <div className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.text}</p>
+    </div>
+  );
+}
+export default Post;
